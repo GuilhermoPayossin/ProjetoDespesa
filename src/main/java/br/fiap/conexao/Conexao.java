df276fd16cd07d448518ccd6642187f6f4e9a975
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-13 22:48:13 -0300

    Adicionando dependencias e conexões ao banco de dados

---
+++ src/main/java/br/fiap/conexao/Conexao.java
@@ -0,0 +1,23 @@
+package br.fiap.conexao;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+    private static String URL = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL";
+    private static String USER = "rm556356";
+    private static String PASSWORD = "010806";
+
+    private Conexao() {}
+
+    public static Connection conectar() {
+        try {
+            DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("Erro ao encontrar o banco\n" + e);
+        }
+        return null;
+    }
+}
